742fe981a5918df6f5f7ec0077d11a45537f7460
huuthanh0312 <huuthanhnguyen0312@gmail.com>
2024-06-25 00:38:38 +0700

    Api user admin

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,13 +21,67 @@ const createUser = asyncHandler(async (req, res) => {
 	const query = { email: user.email };
 	try {
 		const exitsingUser = await User.findOne(query);
-    if(exitsingUser){
-      return res.status(302).json(message: "User already exists!");
-    }
-    const result = await User.create(user);
+		if (exitsingUser) {
+			return res.status(302).json({ message: 'User already exists!' });
+		}
+		const result = await User.create(user);
 		res.status(200).json(result);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 });
-export { getAllUser, createUser };
+
+//@desc  Fetch user
+//@route DELETE /api/user
+//@access private
+const deleteUser = asyncHandler(async (req, res) => {
+	const userId = req.params.id;
+	try {
+		const deleteUser = await User.findByIdAndDelete(userId);
+		if (!deleteUser) {
+			return res.status(302).json({ message: 'User not found!' });
+		}
+		res.status(200).json({ message: 'User deleted successfully' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+//@desc  Fetch user admin
+//@route GET /api/user
+//@access private
+const getAdmin = asyncHandler(async (req, res) => {
+	const email = req.params.email;
+	const query = { email: email };
+	try {
+		const user = await User.findOne(query);
+		if (email !== req.decoded.email) {
+			return res.status(403).send({ message: 'Farbidden access' });
+		}
+		let admin = false;
+		if (user) {
+			admin = user?.role === 'admin';
+		}
+		res.status(200).json(admin);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+//@desc  Fetch make amin user
+//@route PATCH /api/user
+//@access private
+const makeAdmin = asyncHandler(async (req, res) => {
+	const userId = req.params.id;
+	try {
+		const updatedUser = await User.findByIdAndUpdate(userId, { role: 'admin' }, { new: true, runValidators: true });
+		//check not found
+		if (!updatedUser) {
+			return res.status(404).send({ message: 'User not found' });
+		}
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+export { getAllUser, createUser, deleteUser, getAdmin, makeAdmin };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { createUser, getAllUser } from '../controllers/userController.js';
+import { createUser, deleteUser, getAdmin, getAdmin, getAllUser, makeAdmin } from '../controllers/userController.js';
 const router = express.Router();
 
 router.route('/').get(getAllUser);
 router.route('/').post(createUser);
+router.route('/:id').delete(deleteUser);
+router.route('/admin/:email').get(getAdmin);
+router.route('/admin/:id').patch(makeAdmin);
 
 export default router;
